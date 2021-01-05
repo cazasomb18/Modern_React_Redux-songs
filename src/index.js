2cc940c6c17606cc19d413af172b0a6bafb7c6c2
claytonschoonoversmith@gmail.com <claytonschoonoversmith@gmail.com>
2021-01-04 19:46:31 -0600

    finished application and coding challenge 12

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import App from './components/App';
-import { reducers } from './reducers';
+import reducers from './reducers';
 
 ReactDOM.render(
 	<Provider store={createStore(reducers)}>
 		<App/>
 	</Provider>,
-	document.querySelector('#root'));
+	document.querySelector('#root')
+);
